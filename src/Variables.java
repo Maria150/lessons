f133964f163c2b0fe36660a20c3d96252af1ed07
Maria150 <mariay15@mail.ru>
2020-02-26 17:47:12 +0300

    Maria_UlyanovaHw2Task1

---
+++ src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+
+    int numberShipsInPort;
+    int counterOfWholeNumbers;
+    int firstNumber;
+    String answer;
+    double randomNumber;
+    double generatorOfRandomNumbers;
+    String carColor;
+    int carsSpeed;
+    int userEnteredNumber;
+    int biggestNumber;
+    int lovesNumber;
+    int leftBoarderOfRangeOfNumber;
+    int rightBoarderOfRangeOfNumber;
+    int numberThoughtByUser;
+    String possibleAnswer;
+    long bankAccount;
+    double bankBalance;
+    long creditCard;
+    int numberOfWheelsOnBicycle;
+    double ticketPrice;
+}
